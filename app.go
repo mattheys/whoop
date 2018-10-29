91ed563e7fc859be9e88d9665d3e2a4bc64b246d
MJH <matt@matt-heys.co.uk>
2018-10-29 21:26:51 +0000

    Updated app.go

--- app.go
+++ app.go
@@ -21,25 +21,7 @@ func GetStatus(w http.ResponseWriter, _ *http.Request) {
 
 // GetNew returns always the same response.
 func GetNew(w http.ResponseWriter, _ *http.Request) {
-	b := Response{Status: "YesIAmNew"}
-	json.NewEncoder(w).Encode(b)
-}
-
-// GetNew2 returns always the same response.
-func GetNew2(w http.ResponseWriter, _ *http.Request) {
-	b := Response{Status: "YesIAmNew2"}
-	json.NewEncoder(w).Encode(b)
-}
-
-// GetNew3 returns always the same response.
-func GetNew3(w http.ResponseWriter, _ *http.Request) {
-	b := Response{Status: "YesIAmNew3"}
-	json.NewEncoder(w).Encode(b)
-}
-
-// GetNew4 returns always the same response.
-func GetNew4(w http.ResponseWriter, _ *http.Request) {
-	b := Response{Status: "YesIAmNew3"}
+	b := Response{Status: "5"}
 	json.NewEncoder(w).Encode(b)
 }
 
@@ -47,8 +29,5 @@ func main() {
 	router := mux.NewRouter()
 	router.HandleFunc("/status", GetStatus).Methods("GET")
 	router.HandleFunc("/new", GetNew).Methods("GET")
-	router.HandleFunc("/new2", GetNew2).Methods("GET")
-	router.HandleFunc("/new3", GetNew3).Methods("GET")
-	router.HandleFunc("/new4", GetNew4).Methods("GET")
 	log.Fatal(http.ListenAndServe(":8123", router))
 }
